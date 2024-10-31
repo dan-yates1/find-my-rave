ac2896cccb2cfe80cd4c594ac4a0260757c875e5
Daniel Yates <dsyates@live.co.uk>
2024-10-31 17:56:10 +0000

    improved events page layout on mobile

--- src/components/EventCard.tsx
+++ src/components/EventCard.tsx
@@ -19,7 +19,7 @@ const EventCard = ({ event }: EventCardProps) => {
 
   return (
     <div
-      className="flex space-x-4 p-4 rounded-xl mb-6 hover:border hover:border-gray-300 hover:shadow-outline-lg transition-shadow duration-300 ease-in-out cursor-pointer"
+      className="flex flex-col md:flex-row space-x-4 p-4 rounded-xl mb-6 hover:border hover:border-gray-300 transition-shadow duration-300 ease-in-out cursor-pointer"
       onClick={handleCardClick}
     >
       {/* Event Image */}
@@ -28,11 +28,11 @@ const EventCard = ({ event }: EventCardProps) => {
         width={200}
         height={200}
         alt={event.title}
-        className="w-52 h-28 rounded-lg object-cover"
+        className="w-full h-auto md:w-52 md:h-28 rounded-lg object-cover"
       />
 
       {/* Event Details */}
-      <div className="flex-1">
+      <div className="flex-1 my-2 md:my-0">
         <h3 className="text-lg font-semibold">{event.title}</h3>
         <p className="text-gray-500">
           {new Date(event.startDate).toLocaleString("en-GB", {
@@ -46,13 +46,13 @@ const EventCard = ({ event }: EventCardProps) => {
         <div className="flex items-center space-x-2 gap-5 text-blue-600">
           <span className="flex items-center gap-1">
             <MapIcon className="w-5 h-5 text-blue-600" />
-            {event.location}
+            {event.location.trim().split(' ').slice(0, 2).join(' ').replace(/,$/, '')}
           </span>
         </div>
       </div>
 
       {/* Bookmark Icon */}
-      <div className="ml-auto flex items-center">
+      <div className="ml-auto flex items-center hidden">
         <div className="p-2 rounded-full hover:bg-blue-100 transition-colors duration-300">
           <BookmarkIcon className="h-5 w-5 text-blue-500 hover:text-blue-700" />
         </div>
